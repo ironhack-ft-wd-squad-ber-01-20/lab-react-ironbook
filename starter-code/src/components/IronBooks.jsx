1a339a0e3cbf2679663c6911f327607a9ef74594
jonathansaudhof <jonathan@jonathansaudhof.de>
2020-02-20 09:50:42 +0100

    done

--- starter-code/src/components/IronBooks.jsx
+++ starter-code/src/components/IronBooks.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-const LinkedIn = props => {
+const LinkedIn = (props) => {
   return (
     <td>
       <a href={props.url}>
-        <img src="linkedin.png" alt="" style={{ height: "15px" }} />
+        <img src='linkedin.png' alt='' style={{ height: '15px' }} />
       </a>
     </td>
   );
@@ -13,20 +13,81 @@ const LinkedIn = props => {
 export default class IronBooks extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      users: [...this.props.users],
-      studentSearch: false,
-      teacherSearch: false,
-      query: ""
-    };
+      this.INIT_USERS = [...this.props.users];
+      this.state = {
+        users: [...this.props.users],
+        filter: {
+          student: false,
+          teacher: false,
+        },
+        query: '',
+      };
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     console.log(event.type);
+
     this.setState({
-      [event.target.name]: event.target.value
+      filter: { [event.target.name]: !this.state.filter[event.target.name] },
     });
+  };
+
+  handleInput = (event) => {
+    this.setState({
+      query: event.target.value.toLowerCase(),
+    });
+  };
+
+  setUsers(input) {
+    this.setState({
+      users: input,
+    });
+  }
+
+  handleSubmit = (event) => {
+    console.log('submit');
+    console.log(this.state.query);
+    event.preventDefault();
+    if (this.state.query.trim().length !== 0) {
+      this.filterUsers(this.state.query);
+    } else {
+      this.setUsers(this.INIT_USERS);
+    }
+  };
+
+  filterUsers = (filter) => {
+    let filteredUsers = this.state.users;
+    console.log(filter);
+
+    if (filter) {
+      //
+      if (this.state.filter.student) {
+        filteredUsers = this.state.users.filter((element) => {
+          return (
+            element.role === 'student' &&
+            (element.firstName.toLowerCase() === filter ||
+              element.lastName.toLowerCase() === filter)
+          );
+        });
+      } else if (this.state.filter.teacher) {
+        filteredUsers = this.state.users.filter((element) => {
+          return (
+            element.role === 'teacher' &&
+            (element.firstName.toLowerCase() === filter ||
+              element.lastName.toLowerCase() === filter)
+          );
+        });
+      } else {
+        filteredUsers = this.state.users.filter((element) => {
+          return (
+            element.firstName.toLowerCase() === filter ||
+            element.lastName.toLowerCase() === filter
+          );
+        });
+      }
+    }
+
+    this.setUsers(filteredUsers);
   };
 
   render() {
@@ -34,34 +95,34 @@ export default class IronBooks extends Component {
     return (
       <div>
         <h1>IronBooks</h1>
-        <form action="/" onSubmit={this.searchNow}>
+        <form action='/' onSubmit={this.handleSubmit}>
           <div>
             <input
-              type="text"
-              name="query"
-              onChange={this.handleChange}
+              type='text'
+              name='query'
+              onChange={this.handleInput}
               value={this.state.query}
-              placeholder="Seach here..."
+              placeholder='Seach here...'
             />
           </div>
           <input
-            type="checkbox"
+            type='checkbox'
             onChange={this.handleChange}
-            name="studentSearch"
-            checked={this.state.studentSearch.value}
-            id="student-select"
+            name='student'
+            checked={this.state.filter.student}
+            id='student-select'
           />
-          <label htmlFor="student-select"> Student</label>
+          <label htmlFor='student-select'> Student</label>
           <input
-            type="checkbox"
+            type='checkbox'
             onChange={this.handleChange}
-            name="teacherSearch"
-            checked={this.state.studentSearch.value}
-            id="teacher-select"
+            name='teacher'
+            checked={this.state.filter.teacher}
+            id='teacher-select'
           />
-          <label htmlFor="teacher-select"> Teacher</label>
+          <label htmlFor='teacher-select'> Teacher</label>
         </form>
-        <table style={{ textAlign: "left" }}>
+        <table style={{ textAlign: 'left' }}>
           <thead>
             <tr>
               <th>First Name</th>
@@ -72,7 +133,7 @@ export default class IronBooks extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.users.map(element => {
+            {this.state.users.map((element) => {
               return (
                 <tr key={element.id}>
                   <td>{element.firstName}</td>
