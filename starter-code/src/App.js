656fb02a388ab7e0c02f8acf9f8211c97d0fc61a
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-07-15 15:54:11 +0200

    Iteration #1

---
+++ starter-code/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import users from "./users";
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: users
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>IronBook</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>First Name</td>
+              <td>Last Name</td>
+              <td>Campus</td>
+              <td>Role</td>
+              <td>Links</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.users.map(e =>
+              <tr key={e.id}>
+                <td>{e.firstName}</td>
+                <td>{e.lastName}</td>
+                <td>{e.campus}</td>
+                <td>{e.role}</td>
+                <td>{e.linkedin}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
