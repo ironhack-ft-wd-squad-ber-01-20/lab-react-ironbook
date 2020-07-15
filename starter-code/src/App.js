81295e93f27089aa19aef91813457d330b09d040
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-07-15 17:16:06 +0200

    Iteration #2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,13 +7,44 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      users: users
+      users: users,
+      search: ''
     }
   }
+  
+
+  handleNameChange = event => {
+    console.log(event.target.value)
+    this.setState({
+      search: event.target.value
+    })
+  }
+
   render() {
+    const filterUser = this.state.users.filter((name) => {
+      if(this.handleNameChange) {
+        return (
+          name.firstName.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          name.lastName.toLowerCase().includes(this.state.search.toLowerCase())
+        );
+      }
+    })
     return (
       <div className="App">
         <h1>IronBook</h1>
+
+        <form>
+          <label htmlFor="firstName" >Search for First or Last Name</label>
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            value={this.state.search}
+            onChange={this.handleNameChange}
+          />
+        </form>
+
+
         <table>
           <thead>
             <tr>
@@ -26,7 +57,7 @@ class App extends Component {
           </thead>
 
           <tbody>
-            {this.state.users.map(e =>
+            {filterUser.map(e =>
               <tr key={e.id}>
                 <td>{e.firstName}</td>
                 <td>{e.lastName}</td>
